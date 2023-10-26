61ba8a940a53056f64d09e5862b9cf2ef2e04d0a
ShinMinOh <luciano97@naver.com>
2023-10-26 16:41:26 +0900

    [TEST] 정렬 테스트 코드 작성

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -105,11 +105,11 @@ public class QuerydslBasicTest {
                 .selectFrom(member)
                 .fetch();
 
-        //단건 조회
-        Member fetchOne = queryFactory
+       /* //단건 조회
+        Member findMember = queryFactory
                 .selectFrom(member)
                 .fetchOne();
-
+*/
         //처음 한건 조회
         Member fetchFirst = queryFactory
                 .selectFrom(member)
@@ -128,6 +128,36 @@ public class QuerydslBasicTest {
         long count = queryFactory
                 .selectFrom(member)
                 .fetchCount();
+    }
+
+    /**
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순(desc)
+     * 2. 회원 이름 올림차순(asc)
+     * 단 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+     */
+    @Test
+    public void sort(){
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())  //회원 이름이 null인 경우 sort에 의해 맨 마지막.
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+
+
+
     }
 
 }
