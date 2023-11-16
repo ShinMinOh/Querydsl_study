f9b45578175b00117f11e552627c64bdf9e246a9
Shin Min Oh <74702677+ShinMinOh@users.noreply.github.com>
2023-11-16 17:22:11 +0900

    동적 쿼리 - WhereParam 사용 (#11)
    * 조인과 세타조인
    
    * on절을 이용한 조인
    
    * on절을 이용한 조인 세타조인
    
    * 조인-페치 조인
    
    * 서브쿼리-나이가 가장 많은 회원 조회(eq,max)
    
    * 서브쿼리-나이가 평균 이상인 회원 조회(goe,avg)
    
    * 서브쿼리-나이가 10살 이상인 회원 조회(in,gt)
    
    * 서브쿼리-select 절 안에서 서브쿼리 사용
    
    * CaseBuilder를 사용한 복잡한 case문 작성
    
    * CaseBuilder를 사용한 복잡한 case문 작성
    
    * 상수 더하기(Expressions.constant())
    
    * 문자 더하기(stringValue())
    
    * 프로젝션과 결과 반환 - 기본
    
    * 프로젝션과 결과 반환 - DTO 조회
    
    * Querydsl 빈 생성 - Dto 반환
    
    * 프로젝션과 결과 반환 - @QueryProjection
    
    * 동적 쿼리 - BooleanBuilder 사용
    
    * 동적 쿼리 - WhereParam 사용

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -4,7 +4,9 @@ import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
@@ -646,6 +648,9 @@ public class QuerydslBasicTest {
         }
     }
 
+    /**
+     * 동적 쿼리-BooleanBuilder
+     * */
     @Test
     public void dynamicQuery_BooleanBuilder(){
         String usernameParam = "member1";
@@ -656,6 +661,9 @@ public class QuerydslBasicTest {
         assertThat(result.size()).isEqualTo(1);
     }
 
+    /**
+     * 동적 쿼리-BooleanBuilder
+     * */
     private List<Member> searchMember1(String usernameCond, Integer ageCond) {
 
         BooleanBuilder builder = new BooleanBuilder();
@@ -673,4 +681,49 @@ public class QuerydslBasicTest {
             .fetch();
     }
 
+
+    /**
+     * 동적 쿼리-WhereParam
+     * 장점
+     * 1. BooleanBuilder보다 코드 가독성이 좋다.
+     * 2. 함수끼리 조립을 할 수 있다.
+     * 3. 메서드 재활용이 가능하다.
+     * */
+    @Test
+    public void dynamicQuery_WhereParam(){
+        String usernameParam = "member1";
+        Integer ageParam = 10;
+
+        List<Member> result = searchMember2(usernameParam, ageParam);
+
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    private List<Member> searchMember2(String usernameCond, Integer ageCond) {
+        return queryFactory
+            .selectFrom(member)
+//            .where(usernameEq(usernameCond), ageEq(ageCond))   // 만약 usernameEq(usernameCond) 의 리턴값이 null이면 username에 대한 where조건은 무시되고 age조건만 동적쿼리로 만들어진다.
+            .where(allEq(usernameCond, ageCond))                //  usernameEq와 ageEq 함수를 조립.
+            .fetch();
+    }
+
+    private BooleanExpression usernameEq(String usernameCond) { //Predicate타입보다 BooleanExpression타입으로 지정하는게 나중에 함수를 이용하기가 좋다.
+        if(usernameCond != null){
+            return member.username.eq(usernameCond);
+        } else {
+            return null;
+        }
+    }
+
+    private BooleanExpression ageEq(Integer ageCond) {
+        if(ageCond != null){
+            return member.age.eq(ageCond);
+        } else {
+            return null;
+        }
+    }
+
+    private BooleanExpression allEq(String usernameCond, Integer ageCond){
+        return usernameEq(usernameCond).and(ageEq(ageCond));
+    }
 }
