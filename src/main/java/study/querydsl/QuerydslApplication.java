d1c6a31a9928dd3d13a68e4a5f71cb266068d369
Shin Min Oh <74702677+ShinMinOh@users.noreply.github.com>
2023-12-08 16:28:21 +0900

    순수 JPA 리포지토리와 Querydsl (#15)
    * 조인과 세타조인
    
    * on절을 이용한 조인
    
    * on절을 이용한 조인 세타조인
    
    * 조인-페치 조인
    
    * 서브쿼리-나이가 가장 많은 회원 조회(eq,max)
    
    * 서브쿼리-나이가 평균 이상인 회원 조회(goe,avg)
    
    * 서브쿼리-나이가 10살 이상인 회원 조회(in,gt)
    
    * 서브쿼리-select 절 안에서 서브쿼리 사용
    
    * CaseBuilder를 사용한 복잡한 case문 작성
    
    * CaseBuilder를 사용한 복잡한 case문 작성
    
    * 상수 더하기(Expressions.constant())
    
    * 문자 더하기(stringValue())
    
    * 프로젝션과 결과 반환 - 기본
    
    * 프로젝션과 결과 반환 - DTO 조회
    
    * Querydsl 빈 생성 - Dto 반환
    
    * 프로젝션과 결과 반환 - @QueryProjection
    
    * 동적 쿼리 - BooleanBuilder 사용
    
    * 동적 쿼리 - WhereParam 사용
    
    * 수정 벌크 연산
    
    * 수정 벌크 연산 더하기
    
    * 삭제 벌크 연산
    
    * 삭제 벌크 연산
    
    * Revert "삭제 벌크 연산"
    
    This reverts commit 348983ca1490abd6763b768c92b6fcd120e91289.
    
    * 수정
    
    * abort rebase
    
    * SQL function 호출하기
    
    * SQL function -  lower,upper 호출하기
    
    * 순수 JPA 리포지토리와 Querydsl

--- src/main/java/study/querydsl/QuerydslApplication.java
+++ src/main/java/study/querydsl/QuerydslApplication.java
@@ -1,7 +1,10 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QuerydslApplication {
@@ -10,4 +13,9 @@ public class QuerydslApplication {
 		SpringApplication.run(QuerydslApplication.class, args);
 	}
 
+/*	//Spring Bean으로 queryFactory 등록
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em){
+		return new JPAQueryFactory(em);
+	}*/
 }
