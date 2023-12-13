6631f58ce032cc019985eb930d3677c77fad9c3e
ShinMinOh <luciano97@naver.com>
2023-12-13 14:19:13 +0900

    스프링 데이터 JPA 레포지토리로 변경

---
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package study.querydsl.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.querydsl.entity.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+  //메서드 이름으로 자동으로 JPQL 만듬.
+  //select m from Member m where m.username = ?
+  List<Member> findByUsername(String username);
+}
