8553c114420279b595d822065288d512072f789f
ShinMinOh <luciano97@naver.com>
2023-12-14 14:33:10 +0900

    사용자 정의 레포지토리(Custom,Impl)

--- src/main/java/study/querydsl/repository/MemberRepository.java
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -4,7 +4,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.querydsl.entity.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
 
   //메서드 이름으로 자동으로 JPQL 만듬.
   //select m from Member m where m.username = ?
